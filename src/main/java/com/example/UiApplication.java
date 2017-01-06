105d25bb2429102121927694f12f49bc3d67c799
jason-xu-suz <jason.xu.suz@icloud.com>
2017-01-06 13:37:59 +0800

    Add Navigation to the Angular Application THe Greeting The Login Form The Authentication Process Submitting the Login Form The Currently Authenticated User Handling the Login Request on the Server CSRF Protection Logout

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import com.example.security.CsrfHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +30,37 @@ public class UiApplication {
         model.put("content", "Hello World!");
         return model;
     }
+
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
     }
 
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/user","/resource","/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                    .csrf().csrfTokenRepository(csrfTokenRepository())
+                    .and().
+                    logout();
+        }
+
+        private CsrfTokenRepository csrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -53,4 +53,14 @@ angular.module('hello', ['ngRoute'])
                 }
             });
         };
+
+        $scope.logout = function() {
+            $http.post('logout', {}).success(function() {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            }).error(function(data) {
+                $rootScope.authenticated = false;
+            });
+        }
+
     });
